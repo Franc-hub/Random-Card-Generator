1afa4f3c50ddfec1eedaeabb6339abf8cb6d3391
Franc Cosp <franccospmeyaeso@gmail.com>
2021-05-01 08:15:47 +0000

    git psuh

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-let cards = [];
+let deck = [];
 let colors = ["red", "black"];
 let suits = ["♣", "♦", "♥", "♠"];
 let numbers = [
@@ -42,61 +42,16 @@ window.onload = () => {
   number.appendChild(colorNumber);
   number.style.color = randomColor;
 };
+let btn = document.createElement("BUTTON");
+
+btn.style.fontSize = "20px";
+btn.style.marginTop = "100px";
+btn.innerText = "Create a new card";
+btn.style.color = "red";
+btn.addEventListener("click", () => {
+  alert("Work in progress");
+});
+document.body.appendChild(btn);
 const generateRandom = item => {
   return item[Math.floor(Math.random() * item.length)];
 };
-
-// function deckBuilder() {
-//   const values = [
-//     "A",
-//     "2",
-//     "3",
-//     "4",
-//     "5",
-//     "6",
-//     "7",
-//     "8",
-//     "9",
-//     "10",
-//     "J",
-//     "Q",
-//     "K"
-//   ];
-//   const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
-//   const cards = [];
-//   for (let s = 0; s < suits.length; s++) {
-//     for (let v = 0; v < values.length; v++) {
-//       const value = values[v];
-//       const suit = suits[s];
-//       cards.push({ value, suit });
-//     }
-//   }
-//   return cards;
-// }
-
-// const randomCard = cards => {
-//   const random = Math.floor(Math.random() * 51);
-//   const cardValue = cards[random].value;
-//   const cardSuit = cards[random].suit;
-//   let entity;
-//   cardSuit === "Diamonds"
-//     ? (entity = "&diams;")
-//     : (entity = "&" + cardSuit.toLowerCase() + ";");
-//   const card = document.createElement("DIV");
-//   card.classList.add("card", cardSuit.toLowerCase());
-//   card.innerHTML =
-//     '<span class="card-value-suit top">' +
-//     cardValue +
-//     entity +
-//     "</span>" +
-//     '<span class="card-suit">' +
-//     entity +
-//     "</span>" +
-//     '<span class="card-value-suit bot">' +
-//     cardValue +
-//     entity +
-//     "</span>";
-//   document.body.appendChild(card);
-// };
-// const cards = deckBuilder();
-// randomCard(cards);
